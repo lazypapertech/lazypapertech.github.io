d1f8d23855d6f5b72a7cf57b69b73f3097634c96
lazypapertech <129699942+lazypapertech@users.noreply.github.com>
2024-08-10 03:34:52 +0200

    Add files via upload

---
+++ design/pricing_details.js
@@ -0,0 +1,3 @@
+document.getElementById('size-free').innerHTML = 'Max size 100 MB';
+document.getElementById('size-premium').innerHTML = 'Max size 250MB';
+document.getElementById('size-premium-plus').innerHTML = 'Max size 500 MB';
