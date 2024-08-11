20f3e0e8288d8e9f9bb557602cb6bb6031795eae
lazypapertech <129699942+lazypapertech@users.noreply.github.com>
2024-08-11 06:27:41 +0200

    Update general_9_of_enhanced.js

--- design/general_9_of_enhanced.js
+++ design/general_9_of_enhanced.js
@@ -180,6 +180,18 @@ let xx15=".me";
 
 
 
+document.getElementById('login-btn').innerHTML = 'Login';
+document.getElementById('li-home').innerHTML = 'Home';
+document.getElementById('li-affiliates').innerHTML = 'Affiliates';
+document.getElementById('li-pricing').innerHTML = 'Pricing';
+document.getElementById('li-features').innerHTML = 'Features';
+document.getElementById('li-tutorial').innerHTML = 'How to use';
+document.getElementById('li-questions').innerHTML = 'FAQ';
+
+
+
+
+
 document.getElementById('short_captions').innerHTML = 'Short subtitles';
 document.getElementById('long_captions').innerHTML = 'Long subtitles';
 
