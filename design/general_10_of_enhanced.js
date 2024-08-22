6a91607993bb25435f9afb8f603ca0626754b21c
lazypapertech <129699942+lazypapertech@users.noreply.github.com>
2024-08-22 13:59:27 +0200

    Update general_10_of_enhanced.js

--- design/general_10_of_enhanced.js
+++ design/general_10_of_enhanced.js
@@ -220,7 +220,7 @@ document.querySelector('.percentage').innerHTML = '0%';
 document.querySelector('.loader-description').innerHTML = 'Loading . . .';
 
 document.getElementById('editCaptions').innerHTML = `<i class="fas fa-pencil-alt"></i> Edit captions`;
-document.getElementById('createVideo').innerHTML =  `<i class="fas fa-film"></i> Create video`;
+document.getElementById('createVideo').innerHTML =  `<i class="fas fa-film"></i> Add captions`;
 document.querySelector('.error-creation').innerHTML = 'Bad connection. Try it again';
 
 
