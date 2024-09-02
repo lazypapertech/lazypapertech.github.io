38dbade065c53792165c375fad29e256f154c703
lazypapertech <129699942+lazypapertech@users.noreply.github.com>
2024-09-02 14:39:50 +0200

    Add files via upload

---
+++ design/videogenerator_menu.js
@@ -0,0 +1,7 @@
+document.getElementById('login-btn').innerHTML = 'Login';
+document.getElementById('li-home').innerHTML = 'Home';
+document.getElementById('li-affiliates').innerHTML = 'Affiliates';
+document.getElementById('li-pricing').innerHTML = 'Pricing';
+document.getElementById('li-features').innerHTML = 'Features';
+document.getElementById('li-tutorial').innerHTML = 'How to use';
+document.getElementById('li-questions').innerHTML = 'FAQ';
