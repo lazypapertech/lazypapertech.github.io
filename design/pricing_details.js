4eff1ab238d59cb6e695c9a390a5bdcc11864cd1
lazypapertech <129699942+lazypapertech@users.noreply.github.com>
2025-06-15 19:29:13 +0200

    Update pricing_details.js

--- design/pricing_details.js
+++ design/pricing_details.js
@@ -1,15 +1,12 @@
 document.getElementById('price-free').innerHTML = '$0/month';
-document.getElementById('price-premium').innerHTML = '$10/month';
-document.getElementById('price-premium-plus').innerHTML = '$20/month';
+document.getElementById('price-premium').innerHTML = '$20/month';
+document.getElementById('price-premium-plus').innerHTML = '$40/month';
 
 document.getElementById('size-free').innerHTML = 'Max size 100 MB';
-document.getElementById('size-premium').innerHTML = 'Max size 250MB';
-document.getElementById('size-premium-plus').innerHTML = 'Max size 500 MB';
-
-document.getElementById('metamaskTitle').innerHTML = 'Login with Metamask';
-document.getElementById('install-metamask').innerHTML = 'Metamask is not installed, install the extension.';
-document.getElementById('install-on-chrome').innerHTML = 'Install on Chrome';
-
+document.getElementById('size-premium').innerHTML = 'Max size 400 MB';
+document.getElementById('size-premium-plus').innerHTML = 'Max size 900 MB';
+ 
 document.getElementById('privacy_policy').innerHTML = 'Privacy Policy';
 document.getElementById('terms_conditions').innerHTML = 'Terms & Conditions';
 document.querySelector('.copyright').innerHTML = '© 2024 manycaptions.com. All rights reserved';
+
